cb7683454dda08f422e11c189a3aabd6ddc39c25
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-02 19:47:01 +0000

    added objetos

---
+++ Objetos/Objetos/Models/Bebidas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Objetos.Models
+{
+    public class Bebida
+    {
+        public string Name { get; set; }
+        public int Cantidad { get; set; }
+
+        public Bebida(string name, int cantidad)
+        {
+            Name = name;
+            Cantidad = cantidad;
+        }
+
+        public void Beberse(int cantidad)
+        {
+            Cantidad -= cantidad;
+        }
+    }
+}
---
+++ Objetos/Objetos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Objetos.Models;
+
+namespace Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebida bebida = new Bebida("Test", 1000);
+            //bebida.Name = "Test";
+            bebida.Beberse(500);
+            Console.WriteLine(bebida.Cantidad);
+            Console.ReadLine();
+
+
+        }
+    }
+}
