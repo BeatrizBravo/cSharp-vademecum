8d7eb7a3c054cdb266344cb59042180a2bae65f9
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-02 22:01:40 +0000

    change the names to print

--- Objetos/Objetos/Models/Orange.cs
+++ Objetos/Objetos/Models/Orange.cs
@@ -6,7 +6,7 @@ namespace Objetos.Models
     public class Orange : Bebida
     {
         // constructor de su padre y los parametros se los envia al padre y ya no hay que crear un constructor
-        public Orange(int Cantidad, string Nombre="Orange") : base(Nombre, Cantidad)
+        public Orange(int Cantidad, string Nombre="Child-Orange") : base(Nombre, Cantidad)
         {
         }
 
--- Objetos/Objetos/Program.cs
+++ Objetos/Objetos/Program.cs
@@ -7,14 +7,17 @@ namespace Objetos
     {
         static void Main(string[] args)
         {
-            Bebida bebida = new Bebida("Test", 1200); //papa
+            Bebida bebida = new Bebida("Daddy", 1200); 
             //bebida.Name = "Test";
             bebida.Beberse(500);
+            Console.WriteLine(bebida.Name);
             Console.WriteLine(bebida.Cantidad);
+            Console.WriteLine("++++++++++++++");
             /*            Console.ReadLine();
             */
             Orange orange = new Orange(2000); //hijo
             orange.Beberse(500);
+            Console.WriteLine(orange.Name);
             Console.WriteLine(orange.Cantidad);
             Console.ReadLine();
 
