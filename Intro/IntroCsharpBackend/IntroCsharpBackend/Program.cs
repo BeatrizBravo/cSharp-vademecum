d9a3882806135eaad14824b4849d7a980d7a7fd8
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-05 16:19:36 +0000

    added Sale clase and objet

---
+++ Intro/IntroCsharpBackend/IntroCsharpBackend/Program.cs
@@ -0,0 +1,21 @@
+﻿//Sale sale = new Sale();
+var sale = new Sale(15); //azucar sintactica, escribir mas facil y mas legible, no var sino no repetir nombre clase
+var message = sale.GetInfo();
+
+Console.WriteLine(message);
+
+
+class Sale
+{
+    public decimal Total { get; set; }
+
+    //constructor para obligar a que reciba algo
+    public Sale(decimal total)
+    { this.Total = total; }
+
+    public string GetInfo()
+    {
+        return $" Total = {Total}";
+    }
+
+}
