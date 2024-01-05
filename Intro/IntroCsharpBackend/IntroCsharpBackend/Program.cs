ccea4cac4a9a95b313a54c47827b6b6d0ae86056
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-05 16:50:22 +0000

    added herencia with override and overload

--- Intro/IntroCsharpBackend/IntroCsharpBackend/Program.cs
+++ Intro/IntroCsharpBackend/IntroCsharpBackend/Program.cs
@@ -5,7 +5,17 @@ var message = sale.GetInfo();
 Console.WriteLine(message);
 
 
-class Sale
+var saleWithTax = new SaleWithTax(20);
+//var infoTax = saleWithTax.GetInfoWithTax();
+var infoTax = saleWithTax.GetInfo();
+Console.WriteLine(infoTax);
+var getInfoOverload = saleWithTax.GetInfo(4);
+Console.WriteLine(getInfoOverload);
+
+
+
+
+class Sale //papa
 {
     public decimal Total { get; set; }
 
@@ -13,9 +23,41 @@ class Sale
     public Sale(decimal total)
     { this.Total = total; }
 
-    public string GetInfo()
+    public virtual string GetInfo() // para dejar que luego lo OVERRIDE el hijo
     {
-        return $" Total = {Total}";
+        return $"Total without tax = {Total}";
     }
 
+}
+
+class SaleWithTax : Sale //hijo
+{
+    // adding more
+    public decimal Tax { get; set; }
+
+    // sale from creation, father, force to have on element called total
+    //constructor
+    public SaleWithTax(decimal total) : base(total) //mandamos al padre
+    {
+
+    }
+
+    //modificamos el metodo de papa "GetInfo" que tendra la keyword Virtual
+    public override string GetInfo()
+    {
+        return $"Total with Tax = {Total} \t with  Tax = {Tax}";
+    }
+
+    // adding more // ya no es necesario porque modificamos GetInfo ya que hace la misma accion
+    /*    public string GetInfoWithTax()
+        {
+            return $"Total with Tax = {Total} \n Tax = {Tax}";
+        }*/
+
+
+    // sobrecarga, metodo con el mismo nombre pero con diferente entrada de datos
+    public  string GetInfo( int a)
+    {
+        return $"overload parameter is  {a.ToString()}";
+    }
 }
