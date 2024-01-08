b07064e784f164f3e9f0d54e71ee7270596828ac
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-08 14:16:55 +0000

    lambda expressions and anonymous functions

---
+++ Intro/IntroCsharpBackend/FunctionalProgramming/Lambda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    public class Lambda
+    {
+        public int SubtractionCompleta(int a, int b)
+        {
+            int v = a - b;
+            return v;
+        }
+
+
+        public Func<int, int, int> subtractionLambda = (a,b) => a - b;
+
+        //con un solo parametro
+        public Func<int, int> subtractionOneParameter = a => a * 2;
+
+        public Func<int, int, int> SubtractionConMasLineas = (int a, int b) =>
+        {
+            int v = a - b;
+            return v;
+        };
+
+    }
+}
