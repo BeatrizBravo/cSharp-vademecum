3ba53bac9c0f951e8eb58ee9667edf5ad6056e19
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-08 16:40:57 +0000

    added endpoint api/Operation

---
+++ Back/introBack/Controllers/OperationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace introBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    // the endpoint se crea en automatico
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]// especificamos que entramos por get
+        public decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
