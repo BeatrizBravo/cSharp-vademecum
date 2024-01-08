a280a78b28b36c7902557e8a6922c8207d943a19
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-08 21:08:42 +0000

    added People controller with a class People

---
+++ Back/introBack/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace introBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+    }
+
+
+
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+
+
+}
+
