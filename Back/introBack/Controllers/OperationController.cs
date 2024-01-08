3f3c17c033697babac2dfb855754a3c221d8ddd4
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2024-01-08 17:03:37 +0000

    added method get, post, put, delete

--- Back/introBack/Controllers/OperationController.cs
+++ Back/introBack/Controllers/OperationController.cs
@@ -9,10 +9,29 @@ namespace introBack.Controllers
     // the endpoint se crea en automatico
     public class OperationController : ControllerBase
     {
-        [HttpGet]// especificamos que entramos por get
-        public decimal Add(decimal a, decimal b)
+        [HttpGet]// especificamos que entramos por el metodo get: para obtener algo
+        public decimal Get(decimal a, decimal b ) //list all
         {
-            return a + b;
+            return a+b;
         }
+        [HttpPost] //create o sumit data a new resource on the server
+        public decimal Sub(decimal a, decimal b)
+        {
+            return a - b;
+
+        } 
+        [HttpPut] //update o replace o create a new resource in the server
+        public decimal Edit(decimal a, decimal b)
+        {
+            return a * b;
+        } 
+        
+        [HttpDelete] // delete
+        public decimal Delete(decimal a, decimal b)
+        {
+            return a / b;
+        }
+
+        //[HttpPost]//partially update o modify the data, do not create if there is not exits 
     }
 }
